a9fc0bbded91df2e1960c71cdb0cc703cec12e29
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2024-02-18 14:15:45 +0100

    init project

---
+++ src/main/java/ma/youcode/aftasclubapiv2/AftasClubApiV2Application.java
@@ -0,0 +1,13 @@
+package ma.youcode.aftasclubapiv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AftasClubApiV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AftasClubApiV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ma/youcode/aftasclubapiv2/AftasClubApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.aftasclubapiv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AftasClubApiV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
