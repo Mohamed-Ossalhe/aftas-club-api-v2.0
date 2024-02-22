a5a9ae2e17f998c9caf7d3e9e52852a121cde40b
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2024-02-22 10:15:07 +0100

    add generic request and response dtos

---
+++ src/main/java/ma/youcode/aftasclubapiv2/dto/requests/_request.java
@@ -0,0 +1,6 @@
+package ma.youcode.aftasclubapiv2.dto.requests;
+
+import java.io.Serializable;
+
+public interface _request extends Serializable {
+}
---
+++ src/main/java/ma/youcode/aftasclubapiv2/dto/responses/_response.java
@@ -0,0 +1,6 @@
+package ma.youcode.aftasclubapiv2.dto.responses;
+
+import java.io.Serializable;
+
+public interface _response extends Serializable {
+}
